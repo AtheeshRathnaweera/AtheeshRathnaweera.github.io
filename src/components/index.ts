672436f5c58d463c3ce9729c18f95928f7b2621f
Atheesh Rathnaweera <rathnaweeraatheesh72@gmail.com>
2025-09-21 11:24:37 +0530

    feat: reorder exports in index.ts for improved organization

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,8 @@
-export { default as TopNavBar } from "./TopNavBar";
-export { default as SocialSideBar } from "./SocialSideBar";
 export { default as EmailSideBar } from "./EmailSideBar";
+export { default as SiteFooter } from "./SiteFooter";
 export { default as MainContent } from "./MainContent";
 export { default as Section } from "./Section";
-export { default as SectionTitle } from "./SectionTitle";
 export { default as SectionContent } from "./SectionContent";
+export { default as SectionTitle } from "./SectionTitle";
+export { default as SocialSideBar } from "./SocialSideBar";
+export { default as TopNavBar } from "./TopNavBar";
