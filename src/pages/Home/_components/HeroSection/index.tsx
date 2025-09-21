88cfcebd09b5ee721a47ee7f2e0aec4ee35d0068
Atheesh Rathnaweera <rathnaweeraatheesh72@gmail.com>
2025-09-21 12:55:32 +0530

    feat: refactor HeroSection for improved layout and responsiveness

--- src/pages/Home/_components/HeroSection/index.tsx
+++ src/pages/Home/_components/HeroSection/index.tsx
@@ -14,20 +14,14 @@ const HeroSection = () => {
       <h1 className="mt-4 text-4xl font-bold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-7xl dark:text-white">
         Atheesh Rathnaweera.
       </h1>
-      <h3
-        className="mt-4 text-2xl font-semibold dark:text-white leading-relaxed"
-        style={{ maxWidth: "85%" }}
-      >
+      <h3 className="mt-4 text-2xl font-semibold dark:text-white leading-relaxed max-w-full sm:max-w-2xl md:max-w-3xl lg:max-w-4xl">
         I’m a{" "}
         <span className="text-4xl font-semibold dark:text-white">
           Full-Stack Developer
         </span>{" "}
         from Sri Lanka who loves building intuitive digital experiences.
       </h3>
-      <p
-        className="mt-4 text-gray-500 dark:text-gray-400 leading-relaxed"
-        style={{ maxWidth: "60%" }}
-      >
+      <p className="mt-4 text-gray-500 dark:text-gray-400 leading-relaxed max-w-full sm:max-w-xl md:max-w-2xl lg:max-w-3xl">
         I’m passionate about learning new technologies and writing clean,
         maintainable code—I enjoy exploring random projects in my GitHub
         playground. Currently working as a Senior Software Engineer, I’m focused
