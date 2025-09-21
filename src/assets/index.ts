4f6049f5f2d87aee3010ce4a61ce238e04c55e87
Atheesh Rathnaweera <rathnaweeraatheesh72@gmail.com>
2025-09-21 11:08:57 +0530

    feat: update index.html title and favicon, remove unused SVG assets

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,4 +1,5 @@
 export { default as githubIcon } from "./github.svg";
 export { default as linkedinIcon } from "./linkedin.svg";
-export { default as twitterIcon } from "./twitter.svg";
+export { default as mePicture } from "./me_001.jpeg";
 export { default as redditIcon } from "./reddit.svg";
+export { default as twitterIcon } from "./twitter.svg";
